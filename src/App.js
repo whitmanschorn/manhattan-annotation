9533fd4d2044df98a6019280117a066a99ed5f43
whitmanschorn <whitman.schorn@gmail.com>
2015-10-22 09:27:10 -0400

    okay

--- src/App.js
+++ src/App.js
@@ -1,39 +1,95 @@
 import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
-}
+import { Slider } from './slider'
+// import './styles.css';
+// var NumberEditor = require('react-number-editor');
+// var ColorPicker = require('react-colorpicker');
+var _ = require('underscore')
+var CANVAS_HEIGHT = 500
+var CANVAS_WIDTH = 500
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
+
+    constructor(props) {
+
+        super(props);
+        var sliders = {
+            x: {
+                label: 'x',
+                value: CANVAS_WIDTH / 2,
+                min: 0,
+                max: CANVAS_WIDTH,
+                step: 1,
+            },
+            y: {
+                label: 'y',
+                value: CANVAS_HEIGHT / 2,
+                min: 0,
+                max: CANVAS_HEIGHT,
+                step: 1,
+            },
+            z: {
+                label: 'z',
+                value: 1,
+                min: 0.1,
+                max: 5,
+                step: 0.01,
+            },
+            rot: {
+                label: 'rot',
+                value: 0,
+                min: -360,
+                max: 360,
+                step: 1,
+            },
+        }
+
+        var color = '#111111'
+
+        this.state = {sliders: sliders, color: color}
+    }
+
+
+    generateSliderCallback (slider){
+        return _.bind(function (evt) {
+            var newSliders = this.state.sliders
+            newSliders[slider.label].value = evt
+            this.setState({sliders: newSliders})
+            this.forceUpdate()
+        }, this)
+    }
+
+    onChangeColor (evt) {
+        this.setState({color: evt})
+    }
+
+    render() {
+        var sliders = _.map(this.state.sliders, function (slider) {
+            slider.onValueChange = this.generateSliderCallback(slider).bind(this)
+            return (
+                <Slider key={slider.label} {...slider}/>
+                )
+
+        }, this);
+
+        var translateString = 'translate(' + this.state.sliders.x.value + 'px,' + this.state.sliders.y.value + 'px)'
+        var scaleString = 'scale(' + this.state.sliders.z.value + ',' + this.state.sliders.z.value + ')'
+        var rotationString = 'rotate(' + this.state.sliders.rot.value + 'deg)'
+        var sliderStyles = {width: 300, height: 300, transform: translateString + scaleString + rotationString }
+        var fillColor = this.state.color
+        var island = (
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={sliderStyles} viewBox="0 0 300 300">
+                <polygon
+                  style = {{fill: fillColor}}
+                  points = '279.1,160.8 195.2,193.3 174.4,280.8   117.6,211.1 27.9,218.3 76.7,142.7 42.1,59.6 129.1,82.7 197.4,24.1 202.3,114 '/>
+            </svg>)
+
+        return (
+            <div>            
+                <div className="controls">
+                    {sliders}
+                </div>
+                {island}
+            </div>
+            );
+    }
 }
